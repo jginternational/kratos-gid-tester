1f2e0e25a94c0c459d9b80585c4f467911abfe9a
Javi Garate <fjgarate@cimne.upc.edu>
2025-07-15 22:57:33 +0200

    ready

--- scripts/open-window.bat
+++ scripts/open-window.bat
@@ -1,2 +1,3 @@
- %~dp0tester-windows-64.exe -project "%~dp0../project/kratos x64.tester" 
-@REM&"C:/Program Files/GiD/GiD 17.1.4d/gid.exe" -tclsh "P:/GiD/gidproject/tester/tester.tcl" -project "P:/Kratos/kratos-gid-tester/project/kratos x64.tester"
+@REM %~dp0tester-windows-64.exe -project "%~dp0../project/kratos x64.tester" 
+
+&"E:/GiD/GiD 17.1.4d/gid.exe" -tclsh "E:/PROYECTOS/GiD/gid_project/tester/tester.tcl" -project "E:/PROYECTOS/KRATOS/KratosTester/project/kratos x64.tester" 
